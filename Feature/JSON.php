437f7210e23ff44ccf6c22ba1f291109cdf25b40
Peter Seibel <peter@etsy.com>
2013-03-19 11:35:53 -0700

    Cleaning up JSON class.

--- Feature/JSON.php
+++ Feature/JSON.php
@@ -1,37 +1,18 @@
 <?php
 
 /*
- * Utilities for getting existing configs and turning them into
- * JSON-encodeable data.
+ * Utility for turning configs into JSON-encodeable data.
  */
 class Feature_JSON {
 
+    /*
+     * Return the given config stanza as an array that can be json
+     * encoded in a form that is slightly easier to deal with in
+     * Javascript.
+     */
     public static function stanza ($key, $server_config=null) {
         $stanza = self::findStanza($key, $server_config);
         return $stanza !== false ? self::translate($key, $stanza) : false;
-    }
-
-    public static function translate ($key, $stanza) {
-        if (preg_match('/feature_.*_enabled$/', $key)) {
-            return self::translateFeatureFlag($key, $stanza);
-        } else if (substr($key, 0, 3) === 'ab.') {
-            if (self::hasEnabled($stanza)) {
-                return self::translateAB(substr($key, 3), $stanza);
-            }
-        } else if (substr($key, 0, 11) === 'new_config.') {
-            return self::translateNewstyle(substr($key, 11), $stanza);
-        } else {
-            return self::translateOldstyle($key, $stanza);
-        }
-        return false;
-    }
-
-    public static function allKeys () {
-        $server_config = $GLOBALS['server_config'];
-
-        $keys = array();
-        self::walk($server_config, null, $keys);
-        return $keys;
     }
 
     private static function findStanza($key, $cursor) {
@@ -47,39 +28,60 @@ class Feature_JSON {
         return $cursor;
     }
 
-    private static function walk ($config, $path, &$keys) {
-        foreach ($config as $k => $v) {
-            $new_path = $path === null ? $k : "$path.$k";
-            if (self::hasEnabled($v)
-                || preg_match('/^feature_/', $k)
-                || (preg_match('/^new_config\./', $new_path) && self::newstyleEnabled($v))) {
-                $keys[] = $new_path;
-            }
-            if (is_array($v)) {
-                self::walk($v, $new_path, $keys);
+    private static function translate ($key, $value) {
+
+        $spec = self::makeSpec($key);
+
+        $internal_url = true;
+
+        if (is_numeric($value)) {
+            $value = array('enabled' => (int)$value);
+        } else if (is_string($value)) {
+            $value = array('enabled' => $value);
+        }
+
+        $enabled = Std::arrayVar($value, 'enabled', 0);
+        $users   = self::expandUsersOrGroups(Std::arrayVar($value, 'users', array()));
+        $groups  = self::expandUsersOrGroups(Std::arrayVar($value, 'groups', array()));
+
+        if ($enabled === 'off') {
+            $spec['variants'][] = self::makeVariantWithUsersAndGroups('on', 0, $users, $groups);
+            $internal_url = false;
+        } else if (is_numeric($enabled)) {
+            $spec['variants'][] = self::makeVariantWithUsersAndGroups('on', (int)$enabled, $users, $groups);
+        } else if (is_string($enabled)) {
+            $spec['variants'][] = self::makeVariantWithUsersAndGroups($enabled, 100, $users, $groups);
+            $internal_url = false;
+        } else if (is_array($enabled)) {
+            foreach ($enabled as $v => $p) {
+                if (is_numeric($p)) {
+                    // Kind of a kludge. $p had better be numeric and
+                    // there have been configs deployed where it
+                    // wasn't which breaks the Catapult config history
+                    // scripts. This will just skip those.
+                    $spec['variants'][] = self::makeVariantWithUsersAndGroups($v, $p, $users, $groups);
+                }
             }
         }
-    }
+        $spec['internal_url_override'] = $internal_url;
 
-
-    private static function hasEnabled ($x) {
-        return is_array($x) && array_key_exists('enabled', $x);
-    }
-
-    private static function newstyleEnabled ($x) {
-        if (is_array($x)) {
-            return array_key_exists('enabled', $x) ||
-                array_key_exists('admin', $x) ||
-                array_key_exists('internal', $x) ||
-                array_key_exists('public_url_override', $x) ||
-                array_key_exists('users', $x) ||
-                array_key_exists('groups', $x) ||
-                $x === array();
-        } else {
-            return $x === 'on'|| $x === 'off';
-            // Missing winning variants but hard to distinguish them
-            // from, e.g. 'enabled' => 'on'.
+        if (array_key_exists('admin', $value)) {
+            $spec['admin'] = $value['admin'];
         }
+        if (array_key_exists('internal', $value)) {
+            $spec['internal'] = $value['internal'];
+        }
+        if (array_key_exists('bucketing', $value)) {
+            $spec['bucketing'] = $value['bucketing'];
+        }
+        if (array_key_exists('internal', $value)) {
+            $spec['internal'] = $value['internal'];
+        }
+        if (array_key_exists('public_url_override', $value)) {
+            $spec['public_url_override'] = $value['public_url_override'];
+        }
+
+        return $spec;
     }
 
     private static function makeSpec ($key) {
@@ -156,200 +158,4 @@ class Feature_JSON {
         return is_array($x) ? $x : array($x);
     }
 
-    private static function translateNewstyle ($key, $value) {
-
-        $spec = self::makeSpec($key);
-
-        $internal_url = true;
-
-        if (is_numeric($value)) {
-            $value = array('enabled' => (int)$value);
-        } else if (is_string($value)) {
-            $value = array('enabled' => $value);
-        }
-
-        $enabled = Std::arrayVar($value, 'enabled', 0);
-        $users   = self::expandUsersOrGroups(Std::arrayVar($value, 'users', array()));
-        $groups  = self::expandUsersOrGroups(Std::arrayVar($value, 'groups', array()));
-
-        if ($enabled === 'off') {
-            $spec['variants'][] = self::makeVariantWithUsersAndGroups('on', 0, $users, $groups);
-            $internal_url = false;
-        } else if (is_numeric($enabled)) {
-            $spec['variants'][] = self::makeVariantWithUsersAndGroups('on', (int)$enabled, $users, $groups);
-        } else if (is_string($enabled)) {
-            $spec['variants'][] = self::makeVariantWithUsersAndGroups($enabled, 100, $users, $groups);
-            $internal_url = false;
-        } else if (is_array($enabled)) {
-            foreach ($enabled as $v => $p) {
-                if (is_numeric($p)) {
-                    // Kind of a kludge. $p had better be numeric and
-                    // there have been configs deployed where it
-                    // wasn't which breaks the Catapult config history
-                    // scripts. This will just skip those.
-                    $spec['variants'][] = self::makeVariantWithUsersAndGroups($v, $p, $users, $groups);
-                }
-            }
-        }
-        $spec['internal_url_override'] = $internal_url;
-
-        if (array_key_exists('admin', $value)) {
-            $spec['admin'] = $value['admin'];
-        }
-        if (array_key_exists('internal', $value)) {
-            $spec['internal'] = $value['internal'];
-        }
-        if (array_key_exists('bucketing', $value)) {
-            $spec['bucketing'] = $value['bucketing'];
-        }
-        if (array_key_exists('internal', $value)) {
-            $spec['internal'] = $value['internal'];
-        }
-        if (array_key_exists('public_url_override', $value)) {
-            $spec['public_url_override'] = $value['public_url_override'];
-        }
-
-        return $spec;
-    }
-
-    private static function translateFeatureFlag ($key, $value) {
-        $matches = array();
-        if (preg_match('/^(.*)_enabled$/', $key, $matches)) {
-            $k = $matches[1];
-            $spec = self::makeSpec($k);
-            if ($value === 1 || $value === 'on') {
-                $spec['variants'][] = self::makeVariant('on', 100);
-            } elseif ($value === 0 || $value === 'off') {
-                $spec['variant'][] = self::makeVariant('on', 0);
-            }
-            return $spec;
-        }
-        return false;
-    }
-
-    private static function translateOldstyle ($key, $oldstyle) {
-
-        $spec = self::makeSpec($key);
-        $onVariant = self::makeVariant('on', 0);
-
-        if (self::justOn($oldstyle)) {
-            $onVariant['percentage'] = 100;
-
-        } else if (self::justOff($oldstyle)) {
-            $onVariant['percentage'] = 0;
-
-        } else if (is_array($oldstyle) && array_key_exists('enabled', $oldstyle)) {
-
-            if ($oldstyle['enabled'] === 'rampup') {
-                if (array_key_exists('rampup', $oldstyle)) {
-                    $rampup = $oldstyle['rampup'];
-                    if (array_key_exists('percent', $rampup) && $rampup['percent']) {
-                        $onVariant['percentage'] = $rampup['percent'];
-                    }
-                }
-            } else if ($oldstyle['enabled'] === 'on') {
-                $onVariant['percentage'] = 100;
-            } else if ($oldstyle['enabled'] === 'off') {
-                $onVariant['percentage'] = 0;
-            }
-
-            if (array_key_exists('rampup', $oldstyle)) {
-                $rampup = $oldstyle['rampup'];
-
-                if (array_key_exists('admin', $rampup) && self::trueish($rampup['admin'])) {
-                    $spec['admin'] = 'on';
-                }
-
-                if (array_key_exists('internal', $rampup) && $rampup['internal'] === true) {
-                    $spec['internal'] = 'on';
-                }
-
-                if (array_key_exists('random', $rampup) && $rampup['random'] === true) {
-                    $spec['bucketing'] = 'random';
-                }
-
-                if (array_key_exists('whitelist', $rampup)) {
-                    $onVariant['users'] = $rampup['whitelist'];
-                }
-
-                if (array_key_exists('group', $rampup)) {
-                    if (is_array($rampup['group'])) {
-                        $onVariant['groups'] = $rampup['group'];
-                    } else if ($rampup['group'] > 0) {
-                        // Check for group values > 0 since I'm pretty sure
-                        // that's not a legal id. (Came up in one old config,
-                        // at least.)
-                        $onVariant['groups'] = array($rampup['group']);
-                    }
-                }
-            }
-        }
-
-        $spec['variants'][] = $onVariant;
-        return $spec;
-    }
-
-    private static function translateAB ($key, $value) {
-        $spec = self::makeSpec($key);
-        $whitelist_variant = null;
-        $whitelist_users = null;
-
-        if (array_key_exists('whitelist', $value)) {
-            if (!array_key_exists('whitelist_variant', $value)) {
-                Logger::log_info('No whitelist variant in ' . json_encode($value));
-            } else {
-                $whitelist_variant = $value['whitelist_variant'];
-            }
-            $whitelist_users = $value['whitelist'];
-        }
-
-        if ($value['enabled'] === 'on') {
-            $percentages = self::weightsToPercentages($value['weights']);
-
-            foreach ($percentages as $variant => $percentage) {
-                $v = self::makeVariant($variant, $percentage);
-                if ($variant === $whitelist_variant) {
-                    $v['users'] = $whitelist_users;
-                }
-                $spec['variants'][] = $v;
-            }
-
-        } elseif ($value['enabled'] === 'off') {
-            $spec['variants'][] = self::makeVariant('on', 0);
-        }
-
-        if (array_key_exists('admin', $value)) {
-            $spec['admin'] = $value['admin'];
-        }
-
-        return $spec;
-    }
-
-    private static function weightsToPercentages ($weights) {
-
-        $sum = 0;
-        $percentages = array();
-
-        foreach ($weights as $variant => $weight) {
-            $sum += $weight;
-        }
-        foreach ($weights as $variant => $weight) {
-            $percentages[$variant] = 100 * ($sum === 0 ? 0 : $weight/$sum);
-        }
-        return $percentages;
-    }
-
-    private static function justOn ($x) {
-        return is_array($x) && sizeof($x) == 1 && array_key_exists('enabled', $x) &&
-            ($x['enabled'] === 100 || $x['enabled'] === 'on');
-    }
-
-    private static function justOff ($x) {
-        return is_array($x) && sizeof($x) == 1 && array_key_exists('enabled', $x) &&
-            ($x['enabled'] === 0 || $x['enabled'] === 'off');
-    }
-
-    private static function trueish ($x) {
-        return $x === true || $x === 'on' || $x === 'true';
-    }
 }
